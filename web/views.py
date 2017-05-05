3a81758e7f1b3f8d4e30e17d38df4df644493030
De-Great Yartey <yarteydegreat2@gmail.com>
2017-05-05 08:20:26 +0000

    Implemented login

--- web/views.py
+++ web/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django import urls
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as save_login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 
 def admin(request):
     return render(request, "web/index_admin.html")
 
 
+@login_required(login_url="/login/")
 def member(request):
-    return render(request, "web/index_member.html")
+    context = {}
+    return render(request, "web/index_member.html", context=context)
 
 
 def request_confirmation(request):
@@ -22,4 +28,19 @@ def accounts(request):
 
 
 def login(request):
-    return render(request, "web/login.html")
+    if request.user.is_authenticated:
+        return redirect(urls.reverse(member))
+    context = {}
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            save_login(request, user)
+            return redirect(urls.reverse(member))
+
+        if username or password:
+            context = {"error_message": "Username/password combination is incorrect. Please try again!"}
+
+    return render(request, "web/login.html", context=context)
