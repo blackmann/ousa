3ab6425bbe84ace07ac48edc68be2591b5d7a761
De-Great Yartey <yarteydegreat2@gmail.com>
2017-05-06 15:45:42 +0000

    Preparing for deployment

--- old_students_web/urls.py
+++ old_students_web/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
+
+from old_students_web import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r"^", include("web.urls")),
+    url(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.STATIC_ROOT, }),
 ]
